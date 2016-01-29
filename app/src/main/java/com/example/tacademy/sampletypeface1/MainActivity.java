b48743839f21cd0b1c3f05203867a15cdbbaf906
kiss9815 <kiss9815@gmail.com>
2016-01-29 15:09:50 +0900

    전체 화면에서 폰트 사용, 폰트패밀리 등록, 빌드부분과 oncreateView 를 새 액티비티에 복붙해서 그 액티비티를 상속받아 사용하면 그 액티비티에서 사용 가능

--- app/src/main/java/com/example/tacademy/sampletypeface1/MainActivity.java
+++ app/src/main/java/com/example/tacademy/sampletypeface1/MainActivity.java
@@ -1,20 +1,33 @@
 package com.example.tacademy.sampletypeface1;
 
+import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Typeface;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
 import android.support.v7.widget.Toolbar;
+import android.util.AttributeSet;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    /////////////////////////////////////////////////////////////////////////////////////////////////// 나중에 쓸때 복사
+    AppCompatDelegate mDelegate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        if (Build.VERSION.SDK_INT < 11) {
+            getLayoutInflater().setFactory(this);
+        } else {
+            getLayoutInflater().setFactory2(this);
+        }
+        mDelegate = getDelegate();
+/////////////////////////////////////////////////////////////////////////////////////////////////// 나중에 쓸때 복사
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -28,17 +41,39 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-        TextView textView = (TextView)findViewById(R.id.text_namun);
-//        Typeface typeface = Typeface.createFromAsset(getAssets(), "nanumgothic.ttf");  // 각각 typeface해서 폰트 바꾸는 법
-        textView.setTypeface(FontManager.getInstance().getTypeface(this, FontManager.FONT_NAME_NANUM));
-
-        textView = (TextView)findViewById(R.id.text_noto);
-        textView.setTypeface(FontManager.getInstance().getTypeface(this, FontManager.FONT_NAME_NOTO));
-
-        textView = (TextView)findViewById(R.id.text_roboto);
-        textView.setTypeface(FontManager.getInstance().getTypeface(this, FontManager.FONT_NAME_ROBOTO));
+//        TextView textView = (TextView)findViewById(R.id.text_namun);
+////        Typeface typeface = Typeface.createFromAsset(getAssets(), "nanumgothic.ttf");  // 각각 typeface해서 폰트 바꾸는 법
+//        textView.setTypeface(FontManager.getInstance().getTypeface(this, FontManager.FONT_NAME_NANUM));
+//
+//        textView = (TextView)findViewById(R.id.text_noto);
+//        textView.setTypeface(FontManager.getInstance().getTypeface(this, FontManager.FONT_NAME_NOTO));
+//
+//        textView = (TextView)findViewById(R.id.text_roboto);
+//        textView.setTypeface(FontManager.getInstance().getTypeface(this, FontManager.FONT_NAME_ROBOTO));
     }
+/////////////////////////////////////////////////////////////////////////////////////////////////// 나중에 쓸때 복사
+    int[] ids = {android.R.attr.fontFamily};  //텍스트뷰마다 폰트 패밀리를 지정해야 함
 
+    @Override
+    public View onCreateView(String name, Context context, AttributeSet attrs) {
+        View view = super.onCreateView(name, context, attrs);
+        if (view == null) {
+            view = mDelegate.createView(null, name, context, attrs);
+        }
+        if (view == null && name.equals("TextView")) {
+            view = new TextView(context, attrs);
+        }
+        if (view != null && view instanceof TextView) {
+            TypedArray ta = context.obtainStyledAttributes(attrs, ids);
+            String fontName = ta.getString(0);
+            Typeface typeface = FontManager.getInstance().getTypeface(context, fontName);
+            if (typeface != null) {
+                ((TextView) view).setTypeface(typeface);
+            }
+        }
+        return view;
+    }
+    /////////////////////////////////////////////////////////////////////////////////////////////////// 나중에 쓸때 복사
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
